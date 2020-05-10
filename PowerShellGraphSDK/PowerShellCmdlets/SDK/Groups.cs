76a3981f80fe3a5c81818525cc64f81b24c2b290
jackbob2020 <jackbob2020110@outlook.com>
2020-05-10 20:28:02 +0800

    add dev. & add user/group

---
+++ PowerShellGraphSDK/PowerShellCmdlets/SDK/Groups.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+
+
+namespace BC.PowerShellGraphSDK.PowerShellCmdlets
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Cmdlet("Get", "Groups", DefaultParameterSetName = @"Search")]
+    public class Get_Groups : GetOrSearchCmdlet
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [ValidateNotNullOrEmpty]
+        [Parameter(ParameterSetName = @"Get", Mandatory = true, ValueFromPipelineByPropertyName = true, HelpMessage = @"The ID for a &quot;microsoft.graph.group&quot; object in the &quot;groups&quot; collection.")]
+        public System.String groupId { get; set; }
+
+
+        internal override System.String GetResourcePath()
+        {
+            return $"groups/{groupId ?? string.Empty}";
+        }
+    }
+}
